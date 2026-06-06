30016ab7c3af93a0225535de2aab02fc95091063
Brayan <huayllanibryan@gmail.com>
2026-06-05 22:01:16 -0500

    Separé el dashboard de las secciones

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,23 +1,11 @@
 import Navbar from '../components/Navbar'
 import Hero from '../components/Hero'
-import ProblemaAmbiental from '../components/sections/ProblemaAmbiental'
-import Campanas from '../components/sections/Campanas'
-import ComoAyudar from '../components/sections/ComoAyudar'
-import EcoRetos from '../components/sections/EcoRetos'
-import Galeria from '../components/sections/Galeria'
-import Participa from '../components/sections/Participa'
 
 function Home() {
   return (
     <>
       <Navbar />
       <Hero />
-      <ProblemaAmbiental />
-      <Campanas />
-      <ComoAyudar />
-      <EcoRetos />
-      <Galeria />
-      <Participa />
     </>
   )
 }
